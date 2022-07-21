2fef0bb5565893ca17186c9393a91edb09c5e9ce
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-07-21 21:36:12 +0545

    Login UI
    Login UI is created.

---
+++ tiktok/lib/view/screens/auth/login_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok/view/widgets/gitch.dart';
+
+import 'package:tiktok/view/widgets/text_input.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            GlithEffect(
+              child: const Text("Tik Tok",
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white)),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                myIcon: Icons.email_outlined,
+                controller: _emailController,
+                myLabelText: "Email",
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                myIcon: Icons.lock_outlined,
+                controller: _passwordController,
+                myLabelText: "Password",
+                toHide: true,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Text("Login")),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
