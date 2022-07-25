62ee969a3cc16cb2453ec8d1a4031a5f616a2bec
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-07-25 21:41:26 +0545

    Firebase Auth and Storage Setup
    Firebase aut and storage is setup

--- tiktok/lib/control/auth_controller.dart
+++ tiktok/lib/control/auth_controller.dart
@@ -1,5 +1,37 @@
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 
-class AuthControler extends GetxController{
-  
+class AuthControler extends GetxController {
+  // For user register
+
+  void SignUp(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        UserCredential credential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+        _uploadProPic(image);
+      }
+    } catch (e) {
+      print(e);
+      Get.snackbar("Error is Occured", e.toString());
+    }
+  }
+
+  Future<String>_uploadProPic(File image) async {
+    Reference ref = FirebaseStorage.instance
+        .ref()
+        .child("profile pics")
+        .child(FirebaseAuth.instance.currentUser!.uid);
+
+        UploadTask uploadTask = ref.putFile(image);
+        TaskSnapshot snapshot=await uploadTask;
+       String imageDwonloadUrl=await snapshot.ref.getDownloadURL();
+       return imageDwonloadUrl;
+  }
 }
--- tiktok/lib/main.dart
+++ tiktok/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:tiktok/constant.dart';
+import 'package:tiktok/control/auth_controller.dart';
 import 'package:tiktok/view/screens/auth/signup_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthControler());
+  });
   runApp(const MyApp());
 }
 
